91ca8cb3ec891602630b0bf0a2b4196fa54ac7f3
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-05-16 09:15:40 -0300

    Criação da fragment_color.xml.

---
+++ app/src/main/java/com/deitel/doodlz/DoodleView.java
@@ -0,0 +1,14 @@
+package com.deitel.doodlz;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Marcio Albuquerque on 5/15/2016.
+ */
+public class DoodleView extends View {
+    public DoodleView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
