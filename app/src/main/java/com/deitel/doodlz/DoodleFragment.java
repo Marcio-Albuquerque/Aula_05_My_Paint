073f1cb505a565dae619f679722a066812716cc9
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-05-16 15:11:22 -0300

    ---> etapa: Classe interna anônima que implementa a interface SensorEventListener para processar eventos de acelerômetro

--- app/src/main/java/com/deitel/doodlz/DoodleFragment.java
+++ app/src/main/java/com/deitel/doodlz/DoodleFragment.java
@@ -70,5 +70,64 @@ public class DoodleFragment extends Fragment
                 SensorManager.SENSOR_DELAY_NORMAL);;
     }
 
+    // para de detectar eventos de sensor
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        disableAccelerometerListening(); // para de detectar chacoalho
+    }
+
+    // desabilita a detecção de eventos de acelerômetro
+    public void disableAccelerometerListening()
+    {
+        //obtém o componente SensorManager
+        SensorManager sensorManager =
+                (SensorManager)getActivity().getSystemService(
+                        Context.SENSOR_SERVICE);
+
+        //para de detectar eventos de acelerômetro
+        sensorManager.unregisterListener(sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+    }
+
+    // rotina de tratamento para eventos de acelerômetro
+    private SensorEventListener sensorEventListener =
+            new SensorEventListener()
+            {
+                // usa acelerômetro para determeninar se o usuário chacoalhou o dispositivo
+                @Override
+                public void onSensorChanged(SensorEvent event)
+                {
+                    // garante que outras caixas de diálogo não sejam exibidas
+                    if (!dialogOnScreen)
+                    {
+                        // obtém valores x, y e z para componente SensorEvent
+                        float x = event.values[0];
+                        float y = event.values[1];
+                        float z = event.values[2];
+
+                        // salva valor de aceleração anterior
+                        lastAcceleration = currentAcceleration;
+
+                        // calcula a aceleração atual
+                        currentAcceleration = x*x + y*y + z*z;
+
+                        // calcula a mudança na aceleração
+                        acceleration = currentAcceleration *
+                                (currentAcceleration - lastAcceleration);
+
+                        // se a aceleração está acima de determinado limite
+                        if (acceleration > ACCELERATION_THRESHOLD)
+                            confirmErase();
+                    }
+                }// fim do método onSensorChanged
+
+                // método obrigatório da interface SensorEventListener
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy)
+                {
+                }
+            }; //fim da classe interna anônima
 
 } //Final
