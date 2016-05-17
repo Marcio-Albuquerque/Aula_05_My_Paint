ce30bb65fb963c3f48278ada60d5f8522d50a5a2
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-05-17 08:54:49 -0300

    A instrução package e as instruções import de DoodleView

--- app/src/main/java/com/deitel/doodlz/DoodleView.java
+++ app/src/main/java/com/deitel/doodlz/DoodleView.java
@@ -1,8 +1,27 @@
+// DoodleView.java
+// Visualização principal do aplicativo My Paint
 package com.deitel.doodlz;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.print.PrintHelper;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Marcio Albuquerque on 5/15/2016.
